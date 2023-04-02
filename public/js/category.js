fcab1c8c6c78cf85d03f94617c2c4db4471503cc
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2023-04-02 13:10:13 +0530

    chore: load routes category dynamically

---
+++ public/js/category.js
@@ -0,0 +1,42 @@
+const routesContainer = document.getElementById('routes-row');
+
+const fetchAndRenderRoutesCategory = async () => {
+  try {
+    const routesContainer = document.getElementById('routes-row');
+
+    routesContainer.innerHTML = `
+      <div class="d-flex justify-content-center">
+        <div class="spinner-border text-main" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    `;
+
+    const routesRes = await fetch('/js/category.json');
+    const routes = (await routesRes.json()) ?? [];
+
+    routesContainer.innerHTML = routes
+      .map(({ name, description, icon }) => {
+        return `<div class="col-md-4 col-12">
+                <div class="route-card" id="routes-${name}">
+                    <div class="route-card-content">
+                      <h3>${name}</h3>
+                      <p>
+                        ${description}
+                      </p>
+                    </div>
+                    <div class="route-card-icon">
+                      <i class="${icon}"></i>
+                    </div>
+                </div>
+              </div>`;
+      })
+      .join('');
+  } catch (error) {
+    // handle error
+  }
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+  fetchAndRenderRoutesCategory();
+});
